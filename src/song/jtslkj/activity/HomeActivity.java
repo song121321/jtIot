eec643a3bf1cf29f1efc9394333510a0d40f79f1
song121321 <32478000+song121321@users.noreply.github.com>
2018-08-26 13:52:51 +0800

    new action bar

--- src/song/jtslkj/activity/HomeActivity.java
+++ src/song/jtslkj/activity/HomeActivity.java
@@ -45,42 +45,6 @@ public class HomeActivity extends Activity {
                 "    <span style=\"font-size: 20px; font-family: 黑体, SimHei; color: rgb(54, 96, 146);\">&nbsp; 2017年农业水价综合改革项目区，覆盖辖区包括邢家、唐山镇、新城镇、陈庄、索镇、侯庄、周家、果里镇、田庄镇、荆家镇，安装80眼机井水电双控远传计量配套设施。项目实施投入运行后,将有效提高桓台县农业节水灌溉工程现代化管理水平，发挥较好的社会经济效益。</span>\n" +
                 "</p>\n" +
                 "    <span style=\"color: rgb(51, 51, 51); font-family: arial, 宋体, sans-serif; text-indent: 28px; background-color: rgb(255, 255, 255); font-size: 20px;\"><img src=\"http://wlw.jtslkj.cn:8021/UEditor/net/upload/image/20180623/6366534790131929111012666.jpg\" title=\"1057155715_1151257_6.jpg\" alt=\"1057155715_1151257_6.jpg\" width=\"1124\" height=\"662\" style=\"width: 1124px; height: 662px;\"/></span>\n" +
-                "<p style=\"text-align: center;\">\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
-                "<p>\n" +
-                "    <br/>\n" +
-                "</p>\n" +
                 "<p><span style=\"font-family: 宋体;color: rgb(0, 0, 0);letter-spacing: 0;font-size: 80px;background: rgb(255, 255, 255)\"><span style=\"font-family:宋体\">&nbsp; &nbsp; 阎楼镇位于阳谷县中部，距县城</span>9公里，辖53个行政村，辖区总面积67.1平方公里，人口5.1万人、耕地68000亩。2017年农业水价综合改革项目区，覆盖辖区内2.99万亩耕地，安装350眼机井水电双控远传计量配套设施，其中安装水表计量设施的23眼，安装以电折水计量设施的327眼。项目实施投入运行后,将有效提高阎楼镇农业节水灌溉工程现代化管理水平，发挥较好的社会经济效益。</span></p>" +
                 "<p style=\"text-align: center;\">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<img src=\"http://58.57.168.190:3002/UEditor/net/upload/image/20180622/6366527905930335113972383.jpg\" title=\"_副本111.jpg\" alt=\"_副本111.jpg\">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<img src=\"http://58.57.168.190:3002/UEditor/net/upload/image/20180622/6366527907153781497134027.jpg\" title=\"_副本222.jpg\" alt=\"_副本222.jpg\"><br></p>";
         new Thread(new Runnable() {
