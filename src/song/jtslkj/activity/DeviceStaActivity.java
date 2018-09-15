9857afcade46cfdeac001d8597be1f8069ab24be
song121321 <32478000+song121321@users.noreply.github.com>
2018-09-15 22:57:11 +0800

    dialog demo

--- src/song/jtslkj/activity/DeviceStaActivity.java
+++ src/song/jtslkj/activity/DeviceStaActivity.java
@@ -1,8 +1,10 @@
 package song.jtslkj.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -11,6 +13,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.GravityEnum;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
 import com.jtslkj.R;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
@@ -31,6 +37,9 @@ public class DeviceStaActivity extends BaseActivity implements AdapterView.OnIte
     private LinearLayout llBack;
     private TextView tvTitle;
     private List<StaBean> staBeans;
+    private MaterialDialog.Builder mBuilder;
+    private MaterialDialog mMaterialDialog;
+    private String[] stocks = {"海虹控股", "科大讯飞", "中科创达", "掌阅科技", "沃特股份","海虹控股", "科大讯飞", "中科创达", "掌阅科技", "沃特股份","海虹控股", "科大讯飞", "中科创达", "掌阅科技", "沃特股份"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,11 +87,38 @@ public class DeviceStaActivity extends BaseActivity implements AdapterView.OnIte
     }
 
     private void refresh() {
+
+        mBuilder = new MaterialDialog.Builder(DeviceStaActivity.this);
+        mBuilder.title("淘牛指数");
+        mBuilder.titleGravity(GravityEnum.CENTER);
+        mBuilder.theme(Theme.LIGHT);
+        mBuilder.content("针对所有讲师的图文直播、内参、问股及用户搜索热度相关个股利用大数据检索，根据量化策略筛选出对应个股的热度指数，指数越高与讲师关联度越强.(此列表2分钟更新一次)");
+        mMaterialDialog = mBuilder.build();
+        mMaterialDialog.show();
         showLoadingDialog();
         new StaDownLoadTask().execute();
     }
 
     private void setClicker() {
+        tvTitle.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBuilder = new MaterialDialog.Builder(DeviceStaActivity.this);
+                mBuilder.title("选择股票");
+                mBuilder.titleGravity(GravityEnum.CENTER);
+                mBuilder.items(stocks);
+                mBuilder.theme(Theme.LIGHT);
+                mBuilder.autoDismiss(true);
+                mBuilder.itemsCallback(new MaterialDialog.ListCallback() {
+                    @Override
+                    public void onSelection(MaterialDialog dialog, View itemView, int position, CharSequence text) {
+                        Toast.makeText(DeviceStaActivity.this, text, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                mMaterialDialog = mBuilder.build();
+                mMaterialDialog.show();
+            }
+        });
         chooseList.setOnItemClickListener(this);
         mRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
             @Override
@@ -95,10 +131,10 @@ public class DeviceStaActivity extends BaseActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-        String text = chooseList.getItemAtPosition(position) + "";
-        Toast.makeText(this, "position=" + position + "text=" + text,
-                Toast.LENGTH_SHORT).show();
+//
+//        String text = chooseList.getItemAtPosition(position) + "";
+//        Toast.makeText(this, "position=" + position + "text=" + text,
+//                Toast.LENGTH_SHORT).show();
         Intent deviceStaIntent = new Intent(DeviceStaActivity.this,
                 DeviceStaTableActivity.class);
         startActivity(deviceStaIntent);
